53acc83454f1077dc32d05b9c039cababb23deb3
S. Halici <122233382+9winged@users.noreply.github.com>
2024-03-20 09:41:04 +0100

    First progess on creating a GUI for the PassGen.

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -1,30 +1,43 @@
-import java.util.Random;
+import java.security.SecureRandom;
 import java.lang.StringBuilder;
 import java.util.Scanner;
 
 public class PasswordGenerator {
+
+    //array with all characters
     private static final String CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789%+'-/!,$";
-
     static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args)  {
 
-    public static void main(String[] args) {
-
-
+        //user preference
         System.out.println("How long do you want your password to be? (at least 4 characters)");
         int PASSWORD_LENGTH = Integer.parseInt(String.valueOf(scanner.nextInt()));
 
-        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want lowercase letters? (y/n)");
+        String includeLowercase = scanner.nextLine();
+
+        System.out.println("Do you wan uppercase letters? (y/n)");
+        String includeUppercase = scanner.nextLine();
+
+
+
+
+        //secure password generation
+        SecureRandom secureRandom = new SecureRandom();
         StringBuilder passwordBuilder = new StringBuilder();
 
         String realPassword;
 
         while (true) {
             for (int i = 0; i < PASSWORD_LENGTH; i++) {
-                int index = random.nextInt(CHARS.length());
+                int index = secureRandom.nextInt(CHARS.length());
                 char character = CHARS.charAt(index);
                 passwordBuilder.append(character);
             }
 
+
+            //Make sure each character is used at least once
             String password = passwordBuilder.toString();
 
             boolean checkSmall = false;
@@ -55,7 +68,7 @@ public class PasswordGenerator {
 
             passwordBuilder.setLength(0);
         }
-
+        //output the result
         System.out.println("Here is your secure password: " + realPassword);
     }
 }
